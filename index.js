b7ee7676a50a5d15d252d62e691691489822e504
Roman Gusev <rgusev2@gmail.com>
2024-01-31 01:54:07 +0100

    Improve perf

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 export function recaman(n) {
   if (n < 0) return NaN;
   if (n === 0) return 0;
-  const seq = [0];
+  const seq = new Set([0]);
+  let prev = 0;
   for (let i = 1; i <= n; i++) {
-    const prev = seq[i - 1];
-    const next = prev - i;
-    seq[i] = next > 0 && !seq.includes(next) ? next : prev + i;
-    if (i === n) return seq[i];
+    const curr = prev - i > 0 && !seq.has(prev - i) ? prev - i : prev + i;
+    if (i === n) return curr;
+    seq.add(curr);
+    prev = curr;
   }
 }
